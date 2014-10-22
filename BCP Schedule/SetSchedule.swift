927b769af9354a56e763670fc37a0fa9f259512c
Kevin Vincent <kevinsundar@gmail.com>
2014-10-21 21:09:12 -0700

    hell yes

---
+++ BCP Schedule/SetSchedule.swift
@@ -0,0 +1,66 @@
+//
+//  SetSchedule.swift
+//  BCP Schedule
+//
+//  Created by Kevin Vincent on 10/21/14.
+//  Copyright (c) 2014 bcp. All rights reserved.
+//
+
+import UIKit
+
+class SetSchedule: UIViewController {
+    @IBOutlet var c1: UITextField!
+    @IBOutlet var c2: UITextField!
+    @IBOutlet var c3: UITextField!
+    @IBOutlet var c4: UITextField!
+    @IBOutlet var c5: UITextField!
+    @IBOutlet var c6: UITextField!
+    @IBOutlet var c7: UITextField!
+    
+    override func viewDidAppear(animated: Bool) {
+        var error: NSError?
+        var data: String = String(NSUserDefaults.standardUserDefaults().objectForKey("classlist")! as String)
+        var AsArray = JSONParseArray(data)
+        c1.text = AsArray[0] as String;
+        c2.text = AsArray[1] as String;
+        c3.text = AsArray[2] as String;
+        c4.text = AsArray[3] as String;
+        c5.text = AsArray[4] as String;
+        c6.text = AsArray[5] as String;
+        c7.text = AsArray[6] as String;
+    }
+    
+    @IBAction func DoneAction(sender: AnyObject) {
+        //Action before switch
+        var cArray = [c1.text, c2.text, c3.text, c4.text, c5.text, c6.text, c7.text]
+        var toStore:String = JSONStringify(cArray, prettyPrinted: false)
+        NSUserDefaults.standardUserDefaults().setObject(toStore, forKey: "classlist")
+        NSUserDefaults.standardUserDefaults().synchronize();
+        
+        //Switch
+        let vc: AnyObject? = self.storyboard?.instantiateViewControllerWithIdentifier("Main")
+        self.showViewController(vc as ViewController, sender: vc)
+    }
+    
+    func JSONParseArray(jsonString: String) -> [AnyObject] {
+        if let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) {
+            if let array = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: nil)  as? [AnyObject] {
+                return array
+            }
+        }
+        return [AnyObject]()
+    }
+    
+    
+    func JSONStringify(value: AnyObject, prettyPrinted: Bool = false) -> String {
+        var options = prettyPrinted ? NSJSONWritingOptions.PrettyPrinted : nil
+        if NSJSONSerialization.isValidJSONObject(value) {
+            if let data = NSJSONSerialization.dataWithJSONObject(value, options: options, error: nil) {
+                if let string = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                    return string
+                }
+            }
+        }
+        return ""
+    }
+}
--- BCP Schedule/ViewController.swift
+++ BCP Schedule/ViewController.swift
@@ -6,20 +6,81 @@
 //  Copyright (c) 2014 bcp. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var weekDayLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    
+    var myList:[String] = ["asd", "dsa", "derp"];
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        println(NSUserDefaults.standardUserDefaults().objectForKey("classlist"))
+        getData();
+        updateDay();
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myList.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = UITableViewCell();
+        cell.textLabel.text = myList[indexPath.row]
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+    }
+    
+    func updateDay(){
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitWeekday, fromDate: date)
+        let weekdayIndex = components.weekday
+        var listOfDays:[String] = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+        weekDayLabel.text = listOfDays[weekdayIndex-1];
+    }
+    
+    func getData(){
+        let url = NSURL(string: "http://desolate-beach-1823.herokuapp.com/")
+        let request = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            var data:String = NSString(data: data, encoding: NSUTF8StringEncoding)!
+            var dataAsArray:[String] = data.componentsSeparatedByString("-");
+            
+            var cdata: String = String(NSUserDefaults.standardUserDefaults().objectForKey("classlist")! as String)
+            var classArray = self.JSONParseArray(cdata)
+            
+            for (var i = 0; i < dataAsArray.count; i++) {
+                var period = dataAsArray[i]
+                period = "\(period) - \(classArray[period.toInt()! - 1])"
+                dataAsArray[i] = period
+            }
+            
+            self.myList = dataAsArray;
+            self.tableView.reloadData();
+        }
+    }
+    
+    func JSONParseArray(jsonString: String) -> [AnyObject] {
+        if let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) {
+            if let array = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: nil)  as? [AnyObject] {
+                return array
+            }
+        }
+        return [AnyObject]()
+    }
+    
 }
 
